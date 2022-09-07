32e180d33cd2e8fa19e05e6552c2175e29fdfd4f
Simran Rai <simran_rai982@outlook.com>
2022-09-07 14:03:26 +0100

    changed sizing of project items

--- pages/components/ProjectItem.jsx
+++ pages/components/ProjectItem.jsx
@@ -22,18 +22,18 @@ const ProjectItem = ({
         objectFit="contain"
       />
       <div className="hidden  group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-        <h3 className="text-2xl text-white tracking-wider text-center">
+        <h3 className="text-xl text-white tracking-wider text-center">
           {title}
         </h3>
         <p className="pb-4 pt-2 text-white text-center">{tech}</p>
         <a href={projectUrl} target="_blank" rel="noreferrer">
-          <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">
+          <p className="text-center py-2 rounded-lg bg-white text-gray-700 font-bold text-md cursor-pointer">
             More Info
           </p>
         </a>{" "}
         <br />
         <a href={deployUrl} target="_blank" rel="noreferrer">
-          <p className="text-center py-2 rounded-lg bg-white text-gray-700 font-bold text-md cursor-pointer">
+          <p className="text-center py-1 rounded-lg bg-white text-gray-700 font-bold text-sm cursor-pointer">
             See Deployed Version
           </p>
         </a>
